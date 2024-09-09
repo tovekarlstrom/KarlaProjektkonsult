5a8b6660280ee9daf6b8984f27e44153f2fac092
Tove Karlström <tovekarlstrom@hotmail.se>
2024-09-09 11:54:26 +0200

    fix: update configuration of husky to check eslint and prettier

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import React from "react";
-import { useEffect } from "react";
+import Image from 'next/image';
+import React from 'react';
+import { useEffect } from 'react';
 export interface CardProps {
   props: {
     slug: string;
@@ -20,18 +20,18 @@ export interface CardProps {
 export default function Card({ props }: CardProps) {
   const cardStyle: React.CSSProperties =
     props.index % 2 === 0
-      ? { flexDirection: "row" }
-      : { flexDirection: "row-reverse", justifyContent: "space-between" };
+      ? { flexDirection: 'row' }
+      : { flexDirection: 'row-reverse', justifyContent: 'space-between' };
 
   const [windowWidth, setWindowWidth] = React.useState(
-    typeof window !== "undefined" ? window.innerWidth : undefined
+    typeof window !== 'undefined' ? window.innerWidth : undefined,
   );
 
   const [imageSize, setImageSize] = React.useState({ width: 0, height: 0 });
 
   useEffect(() => {
-    if (typeof windowWidth !== "undefined") {
-      console.log("windowWidth", windowWidth < 376);
+    if (typeof windowWidth !== 'undefined') {
+      console.log('windowWidth', windowWidth < 376);
     }
     if (windowWidth) {
       if (windowWidth < 450) {
@@ -47,23 +47,23 @@ export default function Card({ props }: CardProps) {
     // Add event listener for window resize
     const handleResize = () => setWindowWidth(window.innerWidth);
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener('resize', handleResize);
 
     // Cleanup the event listener on component unmount
-    return () => window.removeEventListener("resize", handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, [windowWidth]);
 
   return (
     <div
       style={{
-        backgroundColor: "#F6BC62",
-        maxWidth: "682px",
+        backgroundColor: '#F6BC62',
+        maxWidth: '682px',
 
-        minHeight: "220px",
-        display: "flex",
-        alignItems: "center",
-        margin: "20px",
-        color: "#121212",
+        minHeight: '220px',
+        display: 'flex',
+        alignItems: 'center',
+        margin: '20px',
+        color: '#121212',
 
         ...cardStyle,
       }}
@@ -74,12 +74,12 @@ export default function Card({ props }: CardProps) {
         width={imageSize.width}
         height={imageSize.height}
         sizes="100%"
-        style={{ objectFit: "cover", objectPosition: "center" }}
+        style={{ objectFit: 'cover', objectPosition: 'center' }}
       />
 
-      <div style={{ maxWidth: "50%", padding: "10px", height: "100%" }}>
-        <h3 style={{ fontSize: "16px" }}>{props.heading}</h3>
-        <p style={{ width: "100%", paddingTop: "10px", fontSize: "0.8rem" }}>
+      <div style={{ maxWidth: '50%', padding: '10px', height: '100%' }}>
+        <h3 style={{ fontSize: '16px' }}>{props.heading}</h3>
+        <p style={{ width: '100%', paddingTop: '10px', fontSize: '0.8rem' }}>
           {props.description}
         </p>
       </div>
